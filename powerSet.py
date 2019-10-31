67dca36bed6e42c2c9b8e50ad1ea6784932ca5a2
surafel1999 <surafel1789@gmail.com>
2019-10-31 08:54:05 +0300

    a new version with bugs still lurking

--- powerSet.py
+++ powerSet.py
@@ -9,15 +9,22 @@ default...1,2,3,4,5...12345...empty
 245,345,
 1234,1235,1245,1345,2345
 """
-def powerset(a):
-
-    for i in range(0,len(a)):
-        for j in a[i:]:
-            k=str(a[:i])+str(j)
-            print(k)
-    if(len(a)==1):
-        print("done")
-        return 0
-    else:
-        powerset(a[1:])
-powerset(a)
+def powerSet(lst, res=[], step=1): 
+	'''works for sets of max 4 elements'''
+	if len(lst) < 2:
+		res.append(lst)
+		res.append([])
+		return res
+	for i in range(len(lst)):
+		for j in range(len(lst)):
+			if step == 1 :
+				subset = lst[i: j+1: step]
+			else:
+				subset = lst[i: j: step-1] + lst[j::step]
+			if res.count(subset) == 0:
+				res.append(subset)
+	if step <= len(res):
+		return powerSet(lst, res, step+1)
+	res.sort()
+	return res, len(res)
+print(powerSet(a))
